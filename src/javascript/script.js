95401cde2177dd7064d38db11bba3aa348092bbb
João vitor rodrigues da mata <joao.mata1111@gmail.com>
2024-03-18 21:25:27 -0300

    Update script.js

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -4,6 +4,18 @@ $(document).ready(function() {
         $('#mobile_btn').find('i').toggleClass('fa-x');
     });
 
+    function comprarEbook(type) {
+        var preco = $('#price .current-price span').text();
+
+        if (type === 'link') {
+            alert("Você está sendo redirecionado para a página de compra...");
+            // Aqui você pode adicionar o código para redirecionar para a página de compra
+        } else if (type === 'button') {
+            alert("Você comprou o Ebook por apenas R$ " + preco);
+            // Aqui você pode adicionar o código para processar a compra
+        }
+    }
+
     const sections = $('section');
     const navItems = $('.nav-item');
 
@@ -15,7 +27,7 @@ $(document).ready(function() {
             scrollTop: 0 // Rolagem para o topo da página
         }, 1000); // Duração da animação em milissegundos
     });
-    
+
     $(window).on('scroll', function () {
         const header = $('header');
         const scrollPosition = $(window).scrollTop() - header.outerHeight();
@@ -31,13 +43,13 @@ $(document).ready(function() {
         sections.each(function(i) {
             const section = $(this);
             const sectionTop = section.offset().top - 100;
-            const sectionBottom = sectionTop+ section.outerHeight();
+            const sectionBottom = sectionTop + section.outerHeight();
 
             if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
                 activeSectionIndex = i;
                 return false;
             }
-        })
+        });
 
         navItems.removeClass('active');
         $(navItems[activeSectionIndex]).addClass('active');
@@ -59,11 +71,11 @@ $(document).ready(function() {
         origin: 'left',
         duration: 1000,
         distance: '20%'
-    })
+    });
 
     ScrollReveal().reveal('.feedback', {
         origin: 'right',
         duration: 1000,
         distance: '20%'
-    })
+    });
 });
